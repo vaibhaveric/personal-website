ab7b23aaad08d9e1ae9f68c478872f6b77c9d089
vaibhav.eric <vaibhav.eric@gmail.com>
2022-05-27 10:25:47 +0530

    basic work

--- src/Container/home/index.js
+++ src/Container/home/index.js
@@ -1,7 +1,17 @@
 import react from 'react'
+import './index.css';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+export default function home() {
+    return (<>
+        <body>
+            <div  className='name' >
+            <div>VAIBHAV</div>
+              {/* <br/> */}
+              <span id="spin"></span>
 
-export default function home(){
-    return(<>
-   Hello
+            </div>
+        </body>
     </>)
 }
